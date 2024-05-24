de03093527cf836e173d7caaaaa7a877cfc8bf7d
Artur Mironov <a.mironov.gazauto@ya.ru>
2024-05-24 14:55:51 +0300

    поправил код для соответствия pep8

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,10 @@
 import pytest
 import joblib
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder
-import numpy as np
 
 y_test = pd.read_csv('datasets/y_test.csv')['y_test'].values
 X_test = pd.read_csv('datasets/X_test.csv')
+
 
 @pytest.fixture(scope='module')
 def load_model_and_preprocessors():
@@ -14,6 +13,7 @@ def load_model_and_preprocessors():
     ordinal = joblib.load('scr/ordinal_encoder.pkl')
     label_encoder = joblib.load('scr/label_encoder.pkl')
     return model, scaler, ordinal, label_encoder
+
 
 @pytest.fixture
 def input_data():
@@ -25,17 +25,20 @@ def input_data():
         'subscription_status': ['Yes']
     })
 
+
 def test_scaler(load_model_and_preprocessors, input_data):
     model, scaler, ordinal, label_encoder = load_model_and_preprocessors
     # Проверка масштабирования возраста
     age_scaled = scaler.transform(input_data[['age']])
     assert age_scaled.shape == (1, 1)
 
+
 def test_ordinal_encoder(load_model_and_preprocessors, input_data):
     model, scaler, ordinal, label_encoder = load_model_and_preprocessors
     # Проверка кодирования категориальных признаков
     encoded_data = ordinal.transform(input_data[['gender', 'category', 'size', 'subscription_status']])
     assert encoded_data.shape == (1, 4)
+
 
 def test_prediction(load_model_and_preprocessors, input_data):
     model, scaler, ordinal, label_encoder = load_model_and_preprocessors
@@ -47,7 +50,6 @@ def test_prediction(load_model_and_preprocessors, input_data):
 
     prediction = model.predict(input_data_copy)
     assert prediction[0] in [0, 1]
-
 
     # Предсказания
     y_pred = model.predict(X_test)
